43ab9b3c77c7bbcf0116eceeda6a7ad387abb005
Alex Karadimos <karadalex@gmail.com>
2018-06-16 16:01:41 +0300

    Token distribution graph

--- generators/front-end/templates/src/pages/crowdsale-details.js
+++ generators/front-end/templates/src/pages/crowdsale-details.js
@@ -1,10 +1,38 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Pie from 'react-chartjs-2';
+ 
+const data = {
+  labels: [
+    'Red',
+    'Green',
+    'Yellow'
+  ],
+  datasets: [{
+    data: [300, 50, 100],
+    backgroundColor: [
+      '#FF6384',
+      '#36A2EB',
+      '#FFCE56'
+    ],
+    hoverBackgroundColor: [
+      '#FF6384',
+      '#36A2EB',
+      '#FFCE56'
+    ]
+  }]
+};
 
 const CrowdsaleDetails = () => (
   <div>
     <h1>Crowdsale Details</h1>
     <p>Learn more about this ICO crowdsale</p>
+
+    <h2>Token Distribution</h2>
+    <Pie data = {data}/>
+
+    <h2> Team </h2>
+
     <Link to="/">Go back to the homepage</Link>
   </div>
 )
